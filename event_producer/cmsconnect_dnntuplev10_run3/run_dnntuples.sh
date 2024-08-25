c21e7ccff612201c194169734751b5b3f14cfd88
Congqiao Li <congqiao.li@cern.ch>
2024-08-25 20:40:23 +0200

    DNNTuple v10 routine to reapply puppi v18

--- event_producer/cmsconnect_dnntuplev10_run3/run_dnntuples.sh
+++ event_producer/cmsconnect_dnntuplev10_run3/run_dnntuples.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xe
 
-INPUTFILES=$1
-CMSRUNARGS=$2
-EOSPATH=$3
-BRANCHNAME=$4
+WORKAREA=$1
+INPUTFILES=$2
+CMSRUNARGS=$3
+EOSPATH=$4
+BRANCHNAME=$5
 if [ -z "$BRANCHNAME" ]; then
   BRANCHNAME="dev-Run3-hww"
 fi
@@ -14,18 +15,27 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 ############ Start DNNTuples ############
 export SCRAM_ARCH=el9_amd64_gcc12
-scram p CMSSW CMSSW_14_0_11
-cd CMSSW_14_0_11/src
-eval `scram runtime -sh`
 
-# clone this repo into "DeepNTuples" directory
-git clone https://github.com/colizz/DNNTuples.git DeepNTuples -b $BRANCHNAME
-# download model
-curl -s --retry 10 https://coli.web.cern.ch/coli/tmp/.230626-003937_partv2_model/ak8/V02-HidLayer/model_embed.onnx -o $CMSSW_BASE/src/DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V02-HidLayer/model_embed.onnx
+# setup environment
+if [ "$WORKAREA" == "local" ]; then
+  CMSSWDIR="/afs/cern.ch/user/c/coli/work/hww/inference"
+  cd $CMSSWDIR/CMSSW_14_0_11/src
+  eval `scram runtime -sh`
+else
+  CMSSWDIR="."
+  scram p CMSSW CMSSW_14_0_11
+  cd CMSSW_14_0_11/src
+  eval `scram runtime -sh`
 
-scram b -j8
+  # clone this repo into "DeepNTuples" directory
+  git clone https://github.com/colizz/DNNTuples.git DeepNTuples -b $BRANCHNAME
+  # download model
+  curl -s --retry 10 https://coli.web.cern.ch/coli/tmp/.230626-003937_partv2_model/ak8/V02-HidLayer/model_embed.onnx -o $CMSSW_BASE/src/DeepNTuples/Ntupler/data/InclParticleTransformer-MD/ak8/V02-HidLayer/model_embed.onnx
 
-cd DeepNTuples/Ntupler/test/
+  scram b -j8
+fi
+
+cd $WORKDIR
 
 function retry {
   local n=1
@@ -50,7 +60,7 @@ IFS=',' read -ra ADDR <<< "$INPUTFILES"
 idx=0
 for infile in "${ADDR[@]}"; do
   echo $infile $idx
-  retry cmsRun DeepNtuplizerAK8.py inputFiles=${infile} ${CMSRUNARGS}
+  retry cmsRun $CMSSWDIR/CMSSW_14_0_11/src/DeepNTuples/Ntupler/test/DeepNtuplizerAK8.py inputFiles=${infile} ${CMSRUNARGS}
   mv output.root dnntuple_raw${idx}.root
   idx=$(($idx+1))
 done
@@ -61,9 +71,7 @@ else
 fi
 ### end processing file
 
-mv dnntuple.root ${WORKDIR}/dnntuple.root
-
 if ! [ -z "$EOSPATH" ]; then
-  xrdcp --silent -p -f ${WORKDIR}/dnntuple.root $EOSPATH
+  xrdcp --silent -p -f dnntuple.root $EOSPATH
 fi
 touch ${WORKDIR}/dummy.cc
